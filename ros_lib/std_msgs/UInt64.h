4545598977bf97c207dc7b7bbdfff7b94172d0b8
standmit <standmit@zabstrannik.ru>
2021-01-15 10:36:53 +0700

    migration to ROS Noetic

--- ros_lib/std_msgs/UInt64.h
+++ ros_lib/std_msgs/UInt64.h
@@ -20,7 +20,7 @@ namespace std_msgs
     {
     }
 
-    virtual int serialize(unsigned char *outbuffer) const
+    virtual int serialize(unsigned char *outbuffer) const override
     {
       int offset = 0;
       union {
@@ -36,7 +36,7 @@ namespace std_msgs
       return offset;
     }
 
-    virtual int deserialize(unsigned char *inbuffer)
+    virtual int deserialize(unsigned char *inbuffer) override
     {
       int offset = 0;
       union {
@@ -53,8 +53,8 @@ namespace std_msgs
      return offset;
     }
 
-    const char * getType(){ return "std_msgs/UInt64"; };
-    const char * getMD5(){ return "1b2a79973e8bf53d7b53acb71299cb57"; };
+    virtual const char * getType() override { return "std_msgs/UInt64"; };
+    virtual const char * getMD5() override { return "1b2a79973e8bf53d7b53acb71299cb57"; };
 
   };
 
--- ros_lib/std_msgs/UInt64MultiArray.h
+++ ros_lib/std_msgs/UInt64MultiArray.h
@@ -22,11 +22,11 @@ namespace std_msgs
 
     UInt64MultiArray():
       layout(),
-      data_length(0), data(NULL)
+      data_length(0), st_data(), data(nullptr)
     {
     }
 
-    virtual int serialize(unsigned char *outbuffer) const
+    virtual int serialize(unsigned char *outbuffer) const override
     {
       int offset = 0;
       offset += this->layout.serialize(outbuffer + offset);
@@ -50,7 +50,7 @@ namespace std_msgs
       return offset;
     }
 
-    virtual int deserialize(unsigned char *inbuffer)
+    virtual int deserialize(unsigned char *inbuffer) override
     {
       int offset = 0;
       offset += this->layout.deserialize(inbuffer + offset);
@@ -79,8 +79,8 @@ namespace std_msgs
      return offset;
     }
 
-    const char * getType(){ return "std_msgs/UInt64MultiArray"; };
-    const char * getMD5(){ return "6088f127afb1d6c72927aa1247e945af"; };
+    virtual const char * getType() override { return "std_msgs/UInt64MultiArray"; };
+    virtual const char * getMD5() override { return "6088f127afb1d6c72927aa1247e945af"; };
 
   };
 
